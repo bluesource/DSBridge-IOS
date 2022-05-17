6a849d27c480420fbed9f90fba6c95861c5f7ba5
Stefan Brabenetz <stefan.brabenetz@bluesource.at>
2022-05-17 14:21:08 +0200

    Revert "fix Package.swift"
    This reverts commit 5bb57a46b3f1213d9e80eb401416fed4add0b1fc.

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DSBridge",
-            targets: ["dsbridge"]),
+            targets: ["DSBridge"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "dsbridge",
+            name: "DSBridge",
             dependencies: [])
     ]
 )
